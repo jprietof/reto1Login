a37ed3291639efb0381e5c684361cf58ba1d9649
Jorge Prieto <jprietof@outlook.es>
2021-11-21 20:42:00 -0500

    first commit

---
+++ src/main/java/co/edu/usa/login/Reto1LoginApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1LoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/login/Reto1LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
